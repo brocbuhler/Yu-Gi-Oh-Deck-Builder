473157c92750987f5288acf435c84147fb69a7fd
Broc Buhler <brocbuhler@gmail.com>
2025-05-20 16:47:44 -0700

    Delete Starter-Copy-Repo directory

--- Starter-Copy-Repo/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // React Strict Mode can help identify potential issues but is not required for DevTools.
-  eslint: {
-    dirs: ['pages', 'components', 'lib'], // Run ESLint on specified directories during development
-  },
-};
-
-module.exports = nextConfig;
