47fb054abb2f3f70a45a1e598cc1a2ecaf3eaba6
Broc Buhler <brocbuhler@gmail.com>
2025-06-07 07:45:17 -0700

    fanmade gallery added

--- src/app/page.js
+++ src/app/page.js
@@ -10,29 +10,15 @@ import CardGallery from '../components/CardGallery';
 
 function Home() {
   const [cards, setCards] = useState([]);
-  const [decks, setDecks] = useState([]);
   const { user } = useAuth();
 
   const getGallery = () => {
     getCardGallery(false).then(setCards);
   };
-  
-  const getRandomCardsDeck = () => {
-    let ranDeck = Math.floor((Math.random() * 2)) + 1
-    getCardbyDeck(ranDeck).then(setDecks)
-  };
 
   useEffect(() => {
     getGallery();
-    getRandomCardsDeck();
-    console.log('API KEY:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-    console.log(`This is your uid:`, user.uid)
   }, []);
-  
-  useEffect(() => {
-    console.log(`Here is the card Gallery:`, cards)
-    console.log(`Here is a test deck:`, decks)
-  }, [cards, decks])
 
   return (
     <Row className='g-5'>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,6 +14,9 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
+            <Link className="nav-link" href="/deck/fan">
+              Fan Gallery
+            </Link>
             <Link className="nav-link" href="/card/new">
               New Card
             </Link>
