c2241f0d4356e47587118d4e56194ea3dde403cd
Broc Buhler <brocbuhler@gmail.com>
2025-06-15 09:01:55 -0700

    added a search bar to all relevant pages

--- src/app/deck/fan/page.js
+++ src/app/deck/fan/page.js
@@ -5,14 +5,18 @@ import { useEffect, useState } from "react";
 import { getPublicCards } from "../../../api/cardData";
 import { Col, Row } from "react-bootstrap";
 import CardGallery from '@/components/CardGallery';
+import SearchBar from "../../../components/SearchBar";
 
 function Home() {
   const [cards, setCards] = useState([]);
+  const [searchCards, setSearchCards] = useState([])
+  const [searchState, setSearchState] = useState('')
 
   const getGallery = () => {
     getPublicCards(true).then((fetchedCards) => {
       const cardArray = fetchedCards ? Object.values(fetchedCards) : []
       setCards(cardArray)
+      setSearchCards(cardArray)
     })
   };
 
@@ -20,14 +24,24 @@ function Home() {
     getGallery();
   }, []);
 
+  useEffect(() => {
+    const lowerSearch = searchState.toLowerCase();
+    setSearchCards(
+      cards.filter(card => card.name.toLowerCase().includes(lowerSearch))
+    )
+  }, [searchState, cards])
+
   return (
+    <div>
+    <SearchBar cardList={setSearchState}/>
     <Row className='g-5'>
-      {cards.map(card => (
+      {searchCards.map(card => (
         <Col key={card.firebaseKey} xs={8} sm={6} md={5} lg={4}>
           <CardGallery cardObj={card} update={getGallery}/>
         </Col>
       ))}
     </Row>
+    </div>
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -6,26 +6,42 @@ import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { getCardGallery } from '../api/cardData';
 import CardGallery from '../components/CardGallery';
+import SearchBar from '../components/SearchBar';
 
 function Home() {
   const [cards, setCards] = useState([]);
+  const [searchCards, setSearchCards] = useState([])
+  const [searchState, setSearchState] = useState('')
 
   const getGallery = () => {
-    getCardGallery().then(setCards);
+    getCardGallery().then((fetchedCards) => {
+      setCards(fetchedCards)
+      setSearchCards(fetchedCards)
+    });
   };
 
   useEffect(() => {
     getGallery();
   }, []);
 
+  useEffect(() => {
+    const lowerSearch = searchState.toLowerCase();
+    setSearchCards(
+      cards.filter(card => card.name.toLowerCase().includes(lowerSearch))
+    )
+  }, [searchState, cards])
+
   return (
+    <div>
+    <SearchBar cardList={setSearchState}/>
     <Row className='g-5'>
-      {cards.map(card => (
+      {searchCards.map(card => (
         <Col key={card.firebaseKey} xs={8} sm={6} md={5} lg={4}>
           <CardGallery cardObj={card} update={getGallery}/>
         </Col>
       ))}
     </Row>
+    </div>
   );
 }
 
